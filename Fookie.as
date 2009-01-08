7cb8802f9221a168e902af4e9bcf825e0034ada6
xushengs <xushengs@65b536fe-dd48-11dd-993c-4d20d8c3c59d>
2009-01-08 09:07:28 +0000

    init version

---
+++ Fookie.as
@@ -0,0 +1,178 @@
+﻿package {
+	/**
+	 * @Script:		Fookie.as
+	 * @Licence:	MIT License (http://www.opensource.org/licenses/mit-license.php)
+	 * @Author: 	xushengs@gmail.com
+	 * @Website: 	http://code.google.com/p/fookie/
+	 * @Version: 	0.1
+	 * @Creation: 	Jan 8, 2009
+	 * @Modified: 	Jan 8, 2009
+	 * @Description:
+	 *    This is the main class of Fookie,
+	 *    Use SharedObject to save data to local
+	 */
+	import flash.external.ExternalInterface;
+	import flash.net.SharedObject;
+
+	public class Fookie {		
+		/**
+		 * write data to local
+		 * 
+	 	 * @param key
+	 	 *    the key of the data
+		 * @param dat
+		 *    the value of the data
+		 * @param app
+		 *    the name of your application name,
+		 *    it is the filename of data file
+		 * @param path
+		 *    the path of data, it means who can access the file
+		 * @param exp
+		 *    the number of the day(s) it will expire in
+		 * @param secure
+		 *    use https or not
+	 	 * @return: Object
+		 *    if write success, return the value saved,
+		 *    else return null
+		 */
+		public static function writeData(key:String, dat:String, app:String, path:String, exp:Number, secure:Boolean):String {
+			try {
+				var so:SharedObject = SharedObject.getLocal(app, path, secure);
+				var date:Date = new Date();
+				date.setTime(exp * 24 * 3600 * 1000 + date.time);
+				so.data[key] = dat + '|' + date.time;
+				var fs:String = null;
+				try {
+					fs = so.flush();
+					return dat;
+				} catch (error:Error) {
+					return null;
+				}
+				/*
+				if (fs != null) {
+					switch (fs) {
+						case SharedObjectFlushStatus.PENDING :
+							//trace("Requesting permission to save object...");
+							//so.addEventListener(NetStatusEvent.NET_STATUS, onFlushStatus);
+							//soHandler();
+							break;
+						case SharedObjectFlushStatus.FLUSHED :
+							//trace("Value flushed to disk.");
+							break;
+					}
+				}
+				*/
+			} catch (e:Error) {
+				return null;
+			}
+			return null;
+		}
+		
+		/**
+		 * read data from local data
+		 * 
+	 	 * @param key
+	 	 *    the key of the data you want to read
+		 * @param app
+		 *    the name of your application name,
+		 *   it is the filename of data file
+		 * @param path
+		 *   the path of data, it means who can access the file
+		 * @param secure
+		 *   use https or not
+	 	 * @return: String
+		 *    if read successfully, return the value,
+		 *    else return null
+		 */
+		public static function readData(key:String, app:String, path:String, secure:Boolean):String {
+			try {
+				var so:SharedObject = SharedObject.getLocal(app, path, secure);
+				var dat = so.data[key];
+				// judge whether the data is expired
+				if (dat) {
+					dat = dat.split('|');
+					if (Number(dat[dat.length - 1]) > (new Date()).time) {
+						return dat.slice(0,dat.length - 1).join('');
+					} else {
+						delete so.data[key];
+						return null;
+					}
+				}
+				return null;
+			} catch (e:Error) {
+				return null;
+			}
+			return null;
+		}
+
+		/**
+		 * remove a key/value pair from local data
+		 * 
+	 	 * @param key
+	 	 *    the key of the data you want to remove
+		 * @param app
+		 *    the name of your application name,
+		 *    it is the filename of data file
+		 * @param path
+		 *    the path of data, it means who can access the file
+		 * @param secure
+		 *    use https or not
+	 	 * @return: Boolean
+		 *    if remove successfully, return the value,
+		 *    else return null
+		 */
+		public static function removeData(key:String, app:String, path:String, secure:Boolean):Boolean {
+			try {
+				var so:SharedObject = SharedObject.getLocal(app, path, secure);
+				delete so.data[key];
+				return true;
+			} catch (e:Error) {
+				return false;
+			}
+			return false;
+		}
+
+		/**
+		 * clear all data from local
+		 * 
+		 * @param app
+		 *    the name of your application name,
+		 *    it is the filename of data file
+		 * @param path
+		 *    the path of data, it means who can access the file
+		 * @param secure
+		 *    use https or not
+	 	 * @return: Boolean
+		 *    if clear successfully, return the value,
+		 *    else return null
+		 */
+		public static function clearData(app:String, path:String, secure:Boolean):Boolean {
+			try {
+				var so:SharedObject = SharedObject.getLocal(app, path, secure);
+				so.clear();
+				return true;
+			} catch (e:Error) {
+				return false;
+			}
+			return false;
+		}
+
+		/**
+		 * initialize and notify javascript
+		 * 
+		 */
+		public static function main():void {
+			// add external methods for javascript
+			ExternalInterface.addCallback("write", Fookie.writeData);
+			ExternalInterface.addCallback("read",  Fookie.readData);
+			ExternalInterface.addCallback("remove", Fookie.removeData);
+			ExternalInterface.addCallback("clear", Fookie.clearData);
+			
+			// notify javascript that swf is ready
+			ExternalInterface.call('Fookie.imReady');
+		}
+
+		public function Fookie() {
+		}
+	}
+}
---
+++ fookie.js
@@ -0,0 +1,217 @@
+﻿/**
+ * @Script:     Fookie.js 
+ * @Licence:	MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @Author: 	xushengs@gmail.com
+ * @Website: 	http://code.google.com/p/fookie/
+ * @Version: 	0.1
+ * @Creation: 	Jan 8, 2009
+ * @Modified: 	Jan 8, 2009
+ * @Description:
+ *		A tool class to user swf to save data to local
+ */
+
+var Fookie = {
+    isReady: false,
+    swf: null,
+    swfId: 'fookie_swf_' + Math.round(Math.random() * 100),
+    swfUrl: 'fookie.swf',
+
+    application: 'fookie_data',
+    path: '/',
+    expires: 1000,
+    secure: false,
+
+    onready: function() { },
+
+    // wait for swf to notify me
+    imReady: function() {
+        this.isReady = true;
+        this.swf = document[this.swfId] ? document[this.swfId] : window[this.swfId];
+        this.onready();
+    },
+
+    init: function(url, options) {
+        ///<summary>
+        /// initialize the settings of Fookie,
+        ///  and bind dom ready event to Fookie
+        ///</summary>
+        ///<param name="url" type="string">url: the url of swf</param>
+        ///<param name="options" type="object">
+        /// options: initialize settings of this application, 
+        ///          it could contain these items:
+        ///     onready: this event will fire when Fookie is ready
+        ///     application: the filename of local data file, default is, default is 'fookie_data'
+        ///     path: as same as the cookie, determines who can access, default is '/'
+        ///     expires: the days of data will expire in, default is 1000
+        ///     secure: user https or not, defalt is false
+        ///</param>
+        ///<returns type="String"/>
+
+        if (url) {
+            this.swfUrl = url;
+        }
+
+        options = options || {};
+        this.onready = options.onready ? options.onready : this.onready;
+        this.application = options.application ? options.application : this.application;
+        this.path = options.path ? options.path : this.path;
+        this.expires = options.expires ? options.expires : this.expires;
+        this.secure = options.secure ? true : this.secure;
+
+        this._waitDomReady();
+    },
+
+    write: function(key, dat, options) {
+        ///<summary>
+        /// write data to local, 
+        /// return the value saved, 
+        /// if save faild, return null
+        ///</summary>
+        ///<param name="key" type="string">key: the key of the data</param>
+        ///<param name="dat" type="string">value: the value of the data</param>
+        ///<param name="options" type="object">
+        /// options: could contain these items:
+        ///     application: the filename of local data file, default is, default is 'fookie_data'
+        ///     path: as same as the cookie, determines who can access, default is '/'
+        ///     expires: the days of data will expire in, default is 1000
+        ///     secure: user https or not, defalt is false
+        ///</param>
+        ///<returns type="String"/>
+
+        options = options || {};
+        var a = options.application ? options.application : this.application;
+        var p = options.path ? options.path : this.path;
+        var e = options.expires ? options.expires : this.expires;
+        var s = options.secure ? true : this.secure;
+
+        if (this.swf && this.swf.write) {
+            return this.swf.write(key, dat, a, p, e, s);
+        }
+
+        return null;
+    },
+
+    read: function(key, options) {
+        ///<summary>
+        /// read data from local
+        ///</summary>
+        ///<param name="key" type="string">key: the key of the data</param>
+        ///<param name="options" type="object">
+        /// options: could contain these items:
+        ///     application: the filename of local data file, default is, default is 'fookie_data'
+        ///     path: as same as the cookie, determines who can access, default is '/'
+        ///     secure: user https or not, defalt is false
+        ///</param>
+        ///<returns type="String"/>
+
+        options = options || {};
+        var a = options.application ? options.application : this.application;
+        var p = options.path ? options.path : this.path;
+        var s = options.secure ? true : this.secure;
+
+        if (this.swf && this.swf.read) {
+            return this.swf.read(key, a, p, s);
+        }
+    },
+
+    remove: function(key, options) {
+        ///<summary>
+        /// remove data from local
+        ///</summary>
+        ///<param name="key" type="string">key: the key of the data</param>
+        ///<param name="options" type="object">
+        /// options: could contain these items:
+        ///     application: the filename of local data file, default is, default is 'fookie_data'
+        ///     path: as same as the cookie, determines who can access, default is '/'
+        ///     secure: user https or not, defalt is false
+        ///</param>
+        ///<returns type="String"/>
+
+        options = options || {};
+        var a = options.application ? options.application : this.application;
+        var p = options.path ? options.path : this.path;
+        var s = options.secure ? true : this.secure;
+
+        if (this.swf && this.swf.remove) {
+            return this.swf.remove(key, a, p, s);
+        }
+    },
+
+    clear: function(options) {
+        ///<summary>
+        /// clear all local data in this application
+        ///</summary>
+        ///<param name="options" type="object">
+        /// options: could contain these items:
+        ///     application: the filename of local data file, default is, default is 'fookie_data'
+        ///     path: as same as the cookie, determines who can access, default is '/'
+        ///     secure: user https or not, defalt is false
+        ///</param>
+        ///<returns type="String"/>
+
+        options = options || {};
+        var a = options.application ? options.application : this.application;
+        var p = options.path ? options.path : this.path;
+        var s = options.secure ? true : this.secure;
+
+        if (this.swf && this.swf.clear) {
+            return this.swf.remove(a, p, s);
+        }
+    },
+
+    _writeSwf: function() {
+        var s = [
+                '<object id="', this.swfId, '" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="215" height="138">',
+                  '<param name="movie" value="', this.swfUrl, '" />',
+                  '<param name="quality" value="high" />',
+                  '<param name="allowscriptaccess" value="samedomain" />',
+                  '<embed name="', this.swfId, '" src="', this.swfUrl, '" allowscriptaccess="samedomain" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="215" height="138"></embed>',
+                '</object>'].join('');
+        var div = document.createElement('div');
+        div.style.cssText = 'display:block;border:none;width:1px;height:1px;overflow:hidden;';
+        document.body.appendChild(div);
+        div.innerHTML = s;
+    },
+
+    _waitDomReady: function() {
+        var ua = navigator.userAgent.toLowerCase();
+        var bs = {
+            safari: /webkit/.test(ua),
+            opera: /opera/.test(ua),
+            msie: /msie/.test(ua) && !/opera/.test(ua),
+            mozilla: /mozilla/.test(ua) && !/(compatible|webkit)/.test(ua)
+        };
+
+        if (bs.mozilla || bs.opera) {
+            document.addEventListener("DOMContentLoaded", Fookie._domready, false);
+        }
+        else if (bs.msie) {
+            document.write("<script id=__ie_onload defer=true src=javascript:void(0)></script>");
+
+            var script = document.getElementById("__ie_onload");
+
+            if (script) {
+                script.onreadystatechange = function() {
+                    if (this.readyState == "complete") {
+                        Fookie._domready();
+                    }
+                };
+            }
+
+            script = null;
+        }
+        else if (bs.safari) {
+            Fookie.safariTimer = setInterval(function() {
+                if (document.readyState == "loaded" || document.readyState == "complete") {
+                    clearInterval(Fookie.safariTimer);
+                    Fookie.safariTimer = null;
+                    Fookie._domready();
+                }
+            }, 10);
+        }
+    },
+
+    _domready: function() {
+        Fookie._writeSwf();
+    }
+};
